29be39ef54b8e249cbb88c6fc60217f870d535a0
Longphan2810 <phannhatlong44@gmail.com>
2023-11-01 22:01:38 +0700

    longphantesst2

---
+++ src/main/java/test2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Thinkpad E440
+ */
+public class test2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        // long phan dang test
+    }
+}
