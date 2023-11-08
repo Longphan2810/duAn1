4703731b198d7a9d55da3b7bb1d287cded91eaab
Long Phan <100278075+Longphan2810@users.noreply.github.com>
2023-11-08 21:53:17 +0700

    Update test2.java

--- src/main/java/test2.java
+++ src/main/java/test2.java
@@ -26,13 +26,9 @@ public class test2 {
         // test 2
         //test3
         //test4
-        //test5
-        //test7
-        //test6
-        //test8
-        //test9
-        //test11
+    
         //test10
         // test 100
+        //xxxx
     }
 }
